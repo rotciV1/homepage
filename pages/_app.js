a8f49fdfde49d6c4abc106ff5e573ef01fc259b0
João Victor Santos <jvictor.pereira.santos@gmail.com>
2024-08-08 21:10:24 -0300

    theme support, profile picture, font support

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/layouts/main";
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import Fonts from '../components/fonts'
+import theme from '../libs/theme'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
     </ChakraProvider>
-  );
-};
+  )
+}
 
-export default Website;
+export default Website
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import { Container, Box, Heading, Image } from '@chakra-ui/react'
 
 const Page = () => {
   return (
@@ -7,16 +7,33 @@ const Page = () => {
         Hello, I&apos;m a software developer based in São Paulo, Brasil!
       </Box>
 
-      <Box display={{ md: "flex" }}>
+      <Box display={{ md: 'flex' }}>
         <Box flex={1} p={2} bg="gray.100" borderRadius="md">
           <Heading as="h2" variant="page-title">
             João Victor dos Santos
           </Heading>
           <p>Software Developer (Back-end / Front-end / Data Engineering)</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src={pfp}
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
     </Container>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
